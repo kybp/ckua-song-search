81fc2b8aef8b2018fffced446cc4dbde2ff2500c
Kyle Brown <picokyle@gmail.com>
2017-04-21 16:41:47 -0600

    Add info header

--- src/index.jsx
+++ src/index.jsx
@@ -8,10 +8,24 @@ import reducer from './reducers'
 
 export const beginning = new Date(2014, 6, 5)
 
+const Info = () => (
+  <div className="info">
+    <span className="title">CKUA Song Search</span>
+    <p>
+      Search CKUA's playlists back to 2014. The "L" buttons on search fields can
+      "lock in" the beginning/end of each field. You can click on points in the
+      chart to only display the matching songs for that day.
+    </p>
+  </div>
+)
+
 render(
   <Provider store={ createStore(reducer) }>
     <div>
-      <SearchForm    />
+      <header>
+        <Info />
+        <SearchForm />
+      </header>
       <SearchResults />
     </div>
   </Provider>,
