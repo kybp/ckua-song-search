48509eda3d65136b951554a9626b62e305422791
Kyle Brown <picokyle@gmail.com>
2017-04-28 02:08:08 -0600

    Display max matches in one day instead of songs

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -37,10 +37,12 @@ const getMaxCount = (groups) => (
   )))
 )
 
-const ResultsInfo = ({ totalMatches, maxInOneDay }) => (
+const ResultsInfo = ({ totalMatches, maxInOneDay, numberOfQueries }) => (
   <div className="results-info">
     <span>Total matches in range: { totalMatches }</span>
-    <span>Max matches in one day: { maxInOneDay }</span>
+    <span>Max matches in one day:
+      { numberOfQueries === 0 ? 0 : maxInOneDay / numberOfQueries }
+    </span>
   </div>
 )
 
@@ -65,16 +67,18 @@ const SongList = ({ group }) => {
   )
 }
 
-const SearchResults = ({ loadingSongs, selectedGroup, songSets }) => {
+const SearchResults = ({ loadingSongs, queries, selectedGroup, songSets }) => {
   if (loadingSongs) return <h1>Loading...</h1>
 
-  const groups   = groupByDate(songSets)
-  const maxCount = getMaxCount(groups)
+  const groups          = groupByDate(songSets)
+  const maxCount        = getMaxCount(groups)
+  const numberOfQueries = songSets.length > 0 ? songSets[0].length : 0
 
   return (
     <div>
       <ResultsInfo totalMatches={ songSets.length }
-                   maxInOneDay={ maxCount } />
+                   maxInOneDay={ maxCount }
+                   numberOfQueries={ numberOfQueries } />
       <div className="search-results">
         <SongChart groups={ groups } maxCount={ maxCount } />
         <SongList group={ selectedGroup } />
@@ -83,7 +87,9 @@ const SearchResults = ({ loadingSongs, selectedGroup, songSets }) => {
   )
 }
 
-const mapStateToProps = ({ loadingSongs, selectedGroup, songSets }) => ({
+const mapStateToProps = ({
+  loadingSongs, queries, selectedGroup, songSets
+}) => ({
   selectedGroup: selectedGroup || Array.prototype.concat.apply([], songSets),
   loadingSongs, songSets
 })
