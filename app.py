fa14d0774f3056066cb2b6949cd032dfd54dbeb2
Kyle Brown <picokyle@gmail.com>
2017-05-03 01:47:41 -0600

    Refactor

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from pytz import timezone, UTC
 from sqlalchemy import text
 
 app = Flask(__name__, static_folder='dist')
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/ckua'
 db = SQLAlchemy(app)
 
@@ -26,17 +27,17 @@ class Song(db.Model):
             .order_by(Song.started.asc())\
             .limit(n)
 
-def to_utc(date):
-    return timezone('Canada/Mountain').localize(date).astimezone(UTC)
-
 class JSONSongEncoder(JSONEncoder):
     def default(self, obj):
+        def to_utc(date):
+            return timezone('Canada/Mountain').localize(date).astimezone(UTC)
+
         if isinstance(obj, Song):
             return {
                 'title':   obj.title,
                 'artist':  obj.artist,
                 'album':   obj.album,
-                'started': to_utc(obj.started) }
+                'started': to_utc(obj.started)}
         return super(JSONSongEncoder, self).default(obj)
 app.json_encoder = JSONSongEncoder
 
@@ -59,7 +60,7 @@ does no parsing or validating of the fields listed after 'id' parameters.'''
         elif param_name == 'id':
             if query:
                 result.append(query)
-            query = { 'id': value }
+            query = {'id': value}
         else:
             query[param_name] = value
 
@@ -68,146 +69,145 @@ does no parsing or validating of the fields listed after 'id' parameters.'''
 
     return result
 
-def get_start_and_end(query_string):
-    '''Read the start and end dates from a query string and return a tuple.
-Each date will be a datetime object if it was present, or None if it wasn't.'''
-    start_date = None
-    end_date   = None
+FIELDS = ['artist', 'title', 'album']
 
-    for query_param in query_string.split('&'):
-        param_name, value = split_param(query_param)
-        if param_name == 'start' and value:
-            start_date = datetime.strptime(value, '%Y-%m-%d')
-        elif param_name == 'end' and value:
-            end_date = datetime.strptime(value, '%Y-%m-%d')
+class QueryField():
+    def __init__(self):
+        self.text       = ''
+        self.lock_left  = False
+        self.lock_right = False
 
-    return start_date, end_date
+    def normalized(self):
+        return self.text.lower().strip()
 
-def try_read_lock(parsed_query, unparsed_query, key, field, side):
-    lock_name = 'lock' + side
+def try_read_lock(parsed_query, unparsed_query, key):
+    '''Determine if the `key` into `unparsed_query` represents a lock, and if
+so, save it into `parsed_query`. Return `True` or `False` to indicate whether a
+lock was saved.'''
+    for field in FIELDS:
+        is_locked = unparsed_query[key].lower() == 'true'
 
-    if key == field + '-' + lock_name:
-        parsed_query[field][lock_name] = unparsed_query[key].lower() == 'true'
-        return True
+        if key == field + '-lockLeft':
+            parsed_query[field].lock_left = is_locked
+            return True
+        elif key == field + '-lockRight':
+            parsed_query[field].lock_right = is_locked
+            return True
 
     return False
 
 def parse_query(unparsed_query):
-    fields = ['artist', 'title', 'album']
-    parsed_query = { field: { 'text': '' } for field in fields}
-    parsed_query['id'] = unparsed_query['id']
+    '''Parse a query from a raw dict of parameters into a mapping of the fields
+in `FIELDS` to `QueryField` instances.'''
+    parsed_query = {field: QueryField() for field in FIELDS}
 
     for key in unparsed_query.keys():
-        if key in fields:
-            parsed_query[key]['text'] = unparsed_query[key]
+        if key in FIELDS:
+            parsed_query[key].text = unparsed_query[key]
             continue
-        for field in fields:
-            if try_read_lock(parsed_query, unparsed_query,
-                             key, field, 'Left'):
-                break
-            elif try_read_lock(parsed_query, unparsed_query,
-                               key, field, 'Right'):
-                break
+        try_read_lock(parsed_query, unparsed_query, key)
 
     return parsed_query
 
-def get_search_parameters(query_string):
-    queries = [parse_query(query) for query in split_queries(query_string)]
-    start_date, end_date = get_start_and_end(query_string)
-    return {
-        'queries':    queries,
-        'start_date': start_date,
-        'end_date':   end_date }
+class Search():
+    def __init__(self, queries, start_date, end_date):
+        self.queries    = queries
+        self.start_date = start_date
+        self.end_date   = end_date
 
-def normalize_query_attr(attr):
-    return attr.lower().strip()
+    def validate(self):
+        minimum_term_length = 3
+        def not_long_enough(value):
+            return len(value.text) < minimum_term_length
+        error_message = 'At least one search field per query must ' +\
+                        'have at least {} characters'.\
+                        format(minimum_term_length)
+        if all(map(not_long_enough, self.queries[0].values())):
+            return {'error': error_message}
 
-def find_songs(search_parameters, query):
-    q = Song.query.order_by(Song.started.desc())
+    def perform(self):
+        next_queries = self.queries[1:]
+        results = []
+        n_songs = len(self.queries) - 1
+        for song in self.find_songs(self.queries[0]):
+            next_songs = song.next_songs(n_songs).all()
+            if len(next_songs) < n_songs:
+                continue
+            found_match = True
+            for i in range(n_songs):
+                if not check_song(next_songs[i], next_queries[i]):
+                    found_match = False
+                    break
+            if found_match:
+                results.append([song] + next_songs)
+        return results
 
-    def filter_contains(attr):
-        query_attr = normalize_query_attr(query[attr]['text'])\
-                     .replace('=', '==')\
-                     .replace('%', '=%').replace('_', '=_')
-        if not query_attr:
-            return q
-        if not query[attr]['lockLeft']:
-            query_attr = '%' + query_attr
-        if not query[attr]['lockRight']:
-            query_attr += '%'
-        return q.filter(text("{} ilike :{} escape '='".format(attr, attr)))\
-            .params(**{attr: query_attr})
+    def find_songs(self, query):
+        q = Song.query.order_by(Song.started.desc())
 
-    if 'artist' in query:
-        q = filter_contains('artist')
-    if 'title' in query:
-        q = filter_contains('title')
-    if 'album' in query:
-        q = filter_contains('album')
+        def filter_contains(attr):
+            query_attr = query[attr].normalized()\
+                         .replace('=', '==')\
+                         .replace('%', '=%').replace('_', '=_')
+            if not query_attr:
+                return q
+            if not query[attr].lock_left:
+                query_attr = '%' + query_attr
+            if not query[attr].lock_right:
+                query_attr += '%'
+            return q.filter(text("{} ilike :{} escape '='".format(attr, attr)))\
+                .params(**{attr: query_attr})
 
-    start_date = search_parameters.get('start_date', '')
-    if start_date:
-        q = q.filter(Song.started >= start_date)
-    end_date = search_parameters.get('end_date', '')
-    if end_date:
-        q = q.filter(Song.started <= end_date)
+        for field in FIELDS:
+            if query[field].text:
+                q = filter_contains(field)
 
-    return q
+        if self.start_date:
+            q = q.filter(Song.started >= self.start_date)
+        if self.end_date:
+            q = q.filter(Song.started <= self.end_date)
+
+        return q
+
+    @classmethod
+    def from_query_string(cls, query_string):
+        start_date = None
+        end_date   = None
+
+        for query_param in query_string.split('&'):
+            param_name, value = split_param(query_param)
+            if param_name == 'start' and value:
+                start_date = datetime.strptime(value, '%Y-%m-%d')
+            elif param_name == 'end' and value:
+                end_date = datetime.strptime(value, '%Y-%m-%d')
+
+        queries = [parse_query(query) for query in split_queries(query_string)]
+        return cls(queries, start_date, end_date)
 
 def check_song(song, query):
     def matches(attr):
-        if attr not in query or not query[attr]['text']:
+        query_attr = query[attr].normalized()
+        if not query_attr:
             return True
         model_attr = getattr(song, attr).lower()
-        query_attr = normalize_query_attr(query[attr]['text'])
-        if query[attr]['lockLeft'] and not model_attr.startswith(query_attr):
+        if query[attr].lock_left and not model_attr.startswith(query_attr):
             return False
-        if query[attr]['lockRight'] and not model_attr.endswith(query_attr):
+        if query[attr].lock_right and not model_attr.endswith(query_attr):
             return False
         return query_attr in model_attr
-    if not matches('artist'):
-        return False
-    if not matches('title'):
-        return False
-    if not matches('album'):
-        return False
+
+    for field in FIELDS:
+        if not matches(field):
+            return False
     return True
 
-def validate_queries(queries):
-    minimum_term_length = 3
-    def not_long_enough(value):
-        if not isinstance(value, dict):
-            return True
-        elif 'text' not in value:
-            return False
-        else:
-            return len(value['text']) < minimum_term_length
-    error_message = 'At least one search field per query must ' +\
-                    'have at least {} characters'.format(minimum_term_length)
-    if all(map(not_long_enough, queries[0].values())):
-        return {'error': error_message }
-
 @app.route('/search')
-def search():
-    search_parameters = get_search_parameters(request.query_string.decode())
-    errors = validate_queries(search_parameters['queries'])
-    if errors is not None:
+def search_route():
+    search = Search.from_query_string(request.query_string.decode())
+    errors = search.validate()
+    if errors:
         return jsonify(errors), 400
-    next_queries = search_parameters['queries'][1:]
-    results = []
-    n_songs = len(search_parameters['queries']) - 1
-    for song in find_songs(search_parameters, search_parameters['queries'][0]):
-        next_songs = song.next_songs(n_songs).all()
-        if len(next_songs) < n_songs:
-            continue
-        found_match = True
-        for i in range(n_songs):
-            if not check_song(next_songs[i], next_queries[i]):
-                found_match = False
-                break
-        if found_match:
-            results.append([song] + next_songs)
-    return jsonify(results)
+    return jsonify(search.perform())
 
 @app.route('/')
 def index():
